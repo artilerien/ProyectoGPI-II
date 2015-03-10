350995a2071ab9ef9aaae511fddc2f08b82462cd
linux1 <linux1@localhost>
2015-03-10 18:54:25 +0100

    test iosu

---
+++ ptesting/Iosu San Martin/ParOImpar.java
@@ -0,0 +1,26 @@
+public class ParOImpar
+{
+  boolean Par(int num)
+  {
+    if(num%2==0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  boolean Impar(int num)
+  {
+    if(num%2!=0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
